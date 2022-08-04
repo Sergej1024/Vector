bfa53048938b57ee2590372edc23c5f49187422f
Розум Сергей <sergej_r@list.ru>
2022-08-04 15:16:54 +0500

    Update Vector

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,14 +16,14 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = yandex_vpc_network.network-1.id
 }
 
-resource "yandex_compute_instance" "node" {
+resource "yandex_compute_instance" "node01" {
 
-  count = local.instance_count[terraform.workspace]
-  name  = "${terraform.workspace}-count-${count.index}"
+   name     = "node01"
+   hostname = "clickhouse-01"
 
   resources {
-    cores  = local.vm_cores[terraform.workspace]
-    memory = local.vm_memory[terraform.workspace]
+    cores  = 2
+    memory = 2
   }
 
   boot_disk {
@@ -42,17 +42,30 @@ resource "yandex_compute_instance" "node" {
     #"user-data": "#cloud-config\nusers:\n  - name: sergej\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtmbo9LPEt3EyYAa3cY6tt2hQ2ZRdl9YUdpp+CpW67iVQn7BxRk0xzilSsWg7GzdMQijQx5jhmkeq2dOGuitRnkGEZzKz/zU/ACmlXS9p3P4VIrsDg36QMfy+qjc01V388SQWSNQLU4NVd0EBSLdx/nitqhADSt9iekrfqkhlqGFNvUnCC+bcSswgD1OKXcCWB8gHAzYKfDR4Ck255FRInalOfpkmKTDo/VOjxR919kdKZiy6jslFP1wVZ60M+aa7Ku/07U0lNH56zz7kQArprr4R4AohsLAv2xfPOR6wOMg0Do3T6YXEZia7diMxeDbC+G6rprAMtoglmXEVCpC3Nx2AdFVaTtpKKRoJQOd5AvgzqkT0kj38E0ho1r+c8Y3S3OpZROPzBrnkqkFLW+wfx0z69UyLoDKmmkLq9yUJqLewYaZLzXsIFANb5SEl6LttJ9Beo6Hv1pCNB8dz9ItrH9p63YfoOWhr7duPZbqTvAMB1yJXYzoKwt+96RY3vf4k= sergej@fedora\n      "
   }
 }
-locals {
-  instance_count = {
-    "prod"=2
-    "stage"=1
+
+resource "yandex_compute_instance" "node02" {
+
+   name     = "node02"
+   hostname = "vector-01"
+
+  resources {
+    cores  = 2
+    memory = 2
   }
-  vm_cores = {
-    "prod"=2
-    "stage"=1
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8aqitd4vl5950ihohp"
+    }
   }
-  vm_memory = {
-    "prod"=2
-    "stage"=1
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+    #"user-data": "#cloud-config\nusers:\n  - name: sergej\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtmbo9LPEt3EyYAa3cY6tt2hQ2ZRdl9YUdpp+CpW67iVQn7BxRk0xzilSsWg7GzdMQijQx5jhmkeq2dOGuitRnkGEZzKz/zU/ACmlXS9p3P4VIrsDg36QMfy+qjc01V388SQWSNQLU4NVd0EBSLdx/nitqhADSt9iekrfqkhlqGFNvUnCC+bcSswgD1OKXcCWB8gHAzYKfDR4Ck255FRInalOfpkmKTDo/VOjxR919kdKZiy6jslFP1wVZ60M+aa7Ku/07U0lNH56zz7kQArprr4R4AohsLAv2xfPOR6wOMg0Do3T6YXEZia7diMxeDbC+G6rprAMtoglmXEVCpC3Nx2AdFVaTtpKKRoJQOd5AvgzqkT0kj38E0ho1r+c8Y3S3OpZROPzBrnkqkFLW+wfx0z69UyLoDKmmkLq9yUJqLewYaZLzXsIFANb5SEl6LttJ9Beo6Hv1pCNB8dz9ItrH9p63YfoOWhr7duPZbqTvAMB1yJXYzoKwt+96RY3vf4k= sergej@fedora\n      "
   }
 }
