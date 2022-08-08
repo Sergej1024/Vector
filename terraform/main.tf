e4698af52981dd581e4040558ecc8432036f8f0a
Розум Сергей <sergej_r@list.ru>
2022-08-08 15:06:12 +0500

    08-ansible-02-playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,8 @@ resource "yandex_compute_instance" "node01" {
 
   boot_disk {
     initialize_params {
-      #image_id = "fd8aqitd4vl5950ihohp" moy
-      image_id = "fd879gb88170to70d38a"
+      image_id = "fd8aqitd4vl5950ihohp"
+
     }
   }
 
@@ -39,8 +39,7 @@ resource "yandex_compute_instance" "node01" {
   }
 
   metadata = {
-    ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
-    #"user-data": "#cloud-config\nusers:\n  - name: sergej\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtmbo9LPEt3EyYAa3cY6tt2hQ2ZRdl9YUdpp+CpW67iVQn7BxRk0xzilSsWg7GzdMQijQx5jhmkeq2dOGuitRnkGEZzKz/zU/ACmlXS9p3P4VIrsDg36QMfy+qjc01V388SQWSNQLU4NVd0EBSLdx/nitqhADSt9iekrfqkhlqGFNvUnCC+bcSswgD1OKXcCWB8gHAzYKfDR4Ck255FRInalOfpkmKTDo/VOjxR919kdKZiy6jslFP1wVZ60M+aa7Ku/07U0lNH56zz7kQArprr4R4AohsLAv2xfPOR6wOMg0Do3T6YXEZia7diMxeDbC+G6rprAMtoglmXEVCpC3Nx2AdFVaTtpKKRoJQOd5AvgzqkT0kj38E0ho1r+c8Y3S3OpZROPzBrnkqkFLW+wfx0z69UyLoDKmmkLq9yUJqLewYaZLzXsIFANb5SEl6LttJ9Beo6Hv1pCNB8dz9ItrH9p63YfoOWhr7duPZbqTvAMB1yJXYzoKwt+96RY3vf4k= sergej@fedora\n      "
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
   }
 }
 
@@ -56,8 +55,7 @@ resource "yandex_compute_instance" "node02" {
 
   boot_disk {
     initialize_params {
-      #image_id = "fd8aqitd4vl5950ihohp" moy
-      image_id = "fd879gb88170to70d38a"
+      image_id = "fd8aqitd4vl5950ihohp"
     }
   }
 
@@ -68,6 +66,5 @@ resource "yandex_compute_instance" "node02" {
 
   metadata = {
     ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
-    #"user-data": "#cloud-config\nusers:\n  - name: sergej\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtmbo9LPEt3EyYAa3cY6tt2hQ2ZRdl9YUdpp+CpW67iVQn7BxRk0xzilSsWg7GzdMQijQx5jhmkeq2dOGuitRnkGEZzKz/zU/ACmlXS9p3P4VIrsDg36QMfy+qjc01V388SQWSNQLU4NVd0EBSLdx/nitqhADSt9iekrfqkhlqGFNvUnCC+bcSswgD1OKXcCWB8gHAzYKfDR4Ck255FRInalOfpkmKTDo/VOjxR919kdKZiy6jslFP1wVZ60M+aa7Ku/07U0lNH56zz7kQArprr4R4AohsLAv2xfPOR6wOMg0Do3T6YXEZia7diMxeDbC+G6rprAMtoglmXEVCpC3Nx2AdFVaTtpKKRoJQOd5AvgzqkT0kj38E0ho1r+c8Y3S3OpZROPzBrnkqkFLW+wfx0z69UyLoDKmmkLq9yUJqLewYaZLzXsIFANb5SEl6LttJ9Beo6Hv1pCNB8dz9ItrH9p63YfoOWhr7duPZbqTvAMB1yJXYzoKwt+96RY3vf4k= sergej@fedora\n      "
   }
 }
